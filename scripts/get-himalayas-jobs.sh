285db753439ea212d63071b24c3319c107ffe22f
cawfeecake <cawfeecake@icloud.com>
2023-05-06 04:42:50 -0700

    add summary report to observse endpoint anom.

--- scripts/get-himalayas-jobs.sh
+++ scripts/get-himalayas-jobs.sh
@@ -9,6 +9,8 @@
 set -x
 
 output_file=$1
+echo '# Report about endpoint returns' >> $GITHUB_STEP_SUMMARY
+echo '<ol>' >> $GITHUB_STEP_SUMMARY
 
 current_offset=0
 continue_loop=1
@@ -18,6 +20,7 @@ while [[ $continue_loop == 1 ]]; do
 
   res_limit=$(jq .limit <<< $res)
   res_total_count=$(jq .total_count <<< $res)
+  echo "<li>@ $current_offset, res_limit=$res_limit and res_total_count=$res_total_count</li>" >> $GITHUB_STEP_SUMMARY
   (( current_offset += res_limit ))
   if [[ $current_offset -gt $res_total_count ]]; then
     continue_loop=0
@@ -25,3 +28,5 @@ while [[ $continue_loop == 1 ]]; do
     sleep 1
   fi
 done
+
+echo '</ol>' >> $GITHUB_STEP_SUMMARY
