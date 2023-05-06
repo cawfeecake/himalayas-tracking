6367b25c473056b3fd0dc0d3ebc1f345ddfe9849
cawfeecake <cawfeecake@icloud.com>
2023-05-06 03:55:46 -0700

    update from sh -> bash particulars; fixup script spacing, etc.

--- scripts/get-himalayas-jobs.sh
+++ scripts/get-himalayas-jobs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-## Usage
-# * 1st arg (req.): filename for the resulting jobs json array
+
+##
+# Usage: ./script <output_filepath>
+#
 ##
 
 set -x
@@ -9,28 +11,28 @@ set -x
 # setup user args
 
 jobs_file=$1
-if [ -z "$jobs_file" ]; then
-    echo "Required: filename to put results"
-    exit 1
+if [[ -z "$jobs_file" ]]; then
+  echo "Required: filename to put results"
+  exit 1
 else
-    > $jobs_file
+  > $jobs_file
 fi
 
 # begin API quer(ies)
 
 current_offset=0
 continue_loop=1
-while [ "$continue_loop" -gt 0 ]; do
-    temp_file=$(mktemp)
-    curl -s "https://himalayas.app/jobs/api?offset=$current_offset" > $temp_file
-    cat <<< $(jq -s 'add' $jobs_file <(jq '.jobs' $temp_file)) > $jobs_file
+while [[ $continue_loop > 0 ]]; do
+  temp_file=$(mktemp)
+  curl -s "https://himalayas.app/jobs/api?offset=$current_offset" > $temp_file
+  cat <<< $(jq -s 'add' $jobs_file <(jq '.jobs' $temp_file)) > $jobs_file
 
-    res_limit=$(jq .limit $temp_file)
-    res_total_count=$(jq .total_count $temp_file)
-    (( current_offset += res_limit ))
-    if [ "$current_offset" -ge "$res_total_count" ]; then
-        continue_loop=0
-    else
-        sleep 1
-    fi
+  res_limit=$(jq .limit $temp_file)
+  res_total_count=$(jq .total_count $temp_file)
+  (( current_offset += res_limit ))
+  if [[ $current_offset > $res_total_count ]]; then
+    continue_loop=0
+  else
+    sleep 1
+  fi
 done
